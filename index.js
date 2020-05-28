8b006bdf06032024a6edda9e39474e014fbf2fb9
John K. Speck <speckworks@gmail.com>
2020-05-28 20:58:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let immutable = "hallelujah"
+console.log(immutable)
+console.log(immutable[1])
+
+let arr = []
+arr[1] = "hi from index 1"
+console.log(arr)
+
+immutable[1] = "l"
+console.log(immutable)
